b27938f6633b14a43dccf0fe7e7275bf75194dd9
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2023-03-31 17:33:58 -0300

    Simulate isolated member

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ raft_port = 8008
 for i in range(3):
     member_address = f"127.0.0.1:{raft_port}"
     if len(members) > 0:
-        leader.addNodeToCluster(member_address)
+        if len(members) != 2:
+            leader.addNodeToCluster(member_address)
         member = Member(raft_port, other_members_addresses)
+        if len(members) == 2:
+            # leader.addNodeToCluster(member_address)
+            pass
     else:
         member = Member(raft_port)
         leader = member
